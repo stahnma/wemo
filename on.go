1a02d42057b0beb3d64e80cc180e8353f8c2b3e6
Michael Stahnke <stahnma@puppetlabs.com>
2016-12-18 19:18:45 -0800

    Now works if more than one wemo thing is in use
    Still hard-codes to look for my office lamp, but is slightly more robust
    than hard-coding index 0.

--- on.go
+++ on.go
@@ -19,13 +19,18 @@ index by name maybe?
 
 func main() {
 	switches, err := wemo.Switches()
-	fmt.Println(switches)
 	if err != nil {
 		fmt.Println(err)
 	}
-	if strings.Contains(os.Args[0], "off") {
-		switches[0].Off()
-	} else {
-		switches[0].On()
+	// next if it doesn't contain office
+	for _, device := range switches {
+		//fmt.Println(device.Name)
+		if strings.Contains(device.Name, "Office") {
+			if strings.Contains(os.Args[0], "off") {
+				device.Off()
+			} else {
+				device.On()
+			}
+		}
 	}
 }
